22d7580fdaf26f0b9d3993c6426e1e337ed0c701
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2023-04-27 00:28:44 +0200

    Update LogManager.php : récupération du contexte pour la construction des logs

--- src/Service/LogManager.php
+++ src/Service/LogManager.php
@@ -7,23 +7,49 @@ namespace Service;
 abstract class LogManager
 {
 
+    public static function getContext(): string
+    {
+        $context = '';
+        if (isset($_SERVER['REQUEST_URI'])) {
+            $context = $_SERVER['REQUEST_URI'];
+        }
+
+        if (isset($_SERVER['HTTP_METHOD'])) {
+            $context .= ' - ' . $_SERVER['HTTP_METHOD'];
+        }
+
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $context .= ' - ' . $_SERVER['HTTP_REFERER'];
+        }
+
+        if (isset($_SERVER['HTTP_USER_AGENT'])) {
+            $context .= ' - ' . $_SERVER['HTTP_USER_AGENT'];
+        }
+
+        if (isset($_SERVER['REMOTE_ADDR'])) {
+            $context .= ' - ' . $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $context;
+    }
+
     public static function addInfoLog(string $message): void
     {
-        $file = fopen('../log/test.log', 'a+');
+        $file = fopen('log/info.log', 'a+');
         fwrite($file, date('Y-m-d H:i:s') . ' [INFO] ' . $message . PHP_EOL);
         fclose($file);
     }
 
     public static function addErrorLog(string $message): void
     {
-        $file = fopen('../log/test.log', 'a+');
+        $file = fopen('log/error.log', 'a+');
         fwrite($file, date('Y-m-d H:i:s') . ' [ERROR] ' . $message . PHP_EOL);
         fclose($file);
     }
 
-    public static function getLog(): array
+    public static function getLog($type): array
     {
-        $file = fopen('../log/test.log', 'r');
+        $file = fopen('log/' . $type . '.log', 'r'); // 'r
         $log = [];
         while ($line = fgets($file)) {
             $log[] = $line;
@@ -32,9 +58,9 @@ abstract class LogManager
         return $log;
     }
 
-    public static function emptyLog(): void
+    public static function emptyLog($type): void
     {
-        $file = fopen('../log/test.log', 'w');
+        $file = fopen('log/' . $type . '.log', 'w');
         fclose($file);
     }
 }
