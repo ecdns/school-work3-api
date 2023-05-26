17c91ca1b57a0a527ce12936e55e392013a1ec9e
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2023-05-26 15:39:46 +0200

    Fix log init

--- src/Service/Log.php
+++ src/Service/Log.php
@@ -7,30 +7,6 @@ namespace Service;
 class Log
 {
 
-    private const LOG_DIR = __DIR__ . '/../../log';
-    private const SUCCESS_LOG = self::LOG_DIR . '/success.log';
-    private const ERROR_LOG = self::LOG_DIR . '/error.log';
-
-    public function __construct()
-    {
-        $this->init();
-    }
-
-    private function init(): void
-    {
-        /*if (!file_exists(self::LOG_DIR)) {
-            mkdir(self::LOG_DIR);
-        }*/
-        if (!file_exists(self::SUCCESS_LOG)) {
-            $fichier = fopen(self::SUCCESS_LOG, 'w');
-            fclose($fichier);
-        }
-        if (!file_exists(self::ERROR_LOG)) {
-            $fichier = fopen(self::ERROR_LOG, 'w');
-            fclose($fichier);
-        }
-    }
-
     public function getContext(): string
     {
         $context = '';
@@ -81,14 +57,14 @@ class Log
 
     public function addSuccessLog(string $message): void
     {
-        $file = fopen(self::SUCCESS_LOG, 'a+');
+        $file = fopen('../log/success.log', 'a+');
         fwrite($file, date('Y-m-d H:i:s') . ' - ' . $message . PHP_EOL);
         fclose($file);
     }
 
     public function addErrorLog(string $message): void
     {
-        $file = fopen(self::ERROR_LOG, 'a+');
+        $file = fopen('../log/error.log', 'a+');
         fwrite($file, date('Y-m-d H:i:s') . ' - ' . $message . PHP_EOL);
         fclose($file);
     }
